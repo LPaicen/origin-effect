ecc895de7cd67ee0c0ca0d5b802fddc047f54579
LPaicen <fulipz@qq.com>
2024-04-21 16:34:42 +0800

    test

--- build/build-docs.py
+++ build/build-docs.py
@@ -7,14 +7,11 @@ app = typer.Typer()
 
 docs_path = Path("docs")
 site_path = Path("site")
-artifact = Path("artifacts")
 
 @app.command()
 def build() -> None:
     shutil.rmtree(docs_path, ignore_errors=True)
-    shutil.move(artifact / site_path, Path("/"))
     os.rename(site_path, docs_path)
-    shutil.rmtree(artifact)
 
 if __name__ == "__main__":
     app()
