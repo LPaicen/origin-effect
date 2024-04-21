fc55363fd45d84dec447381dc51c2ac318080e97
LPaicen <fulipz@qq.com>
2024-04-21 15:37:40 +0800

    test

--- build/i18n.py
+++ build/i18n.py
@@ -11,6 +11,7 @@ app = typer.Typer()
 default_lang = "zh"
 docs_path = Path("docs")
 site_path = Path("site").absolute()
+build_site_path = Path("site_build").absolute()
 
 def get_available_lang_paths() -> List[Path]:
     return sorted(docs_path.iterdir())
@@ -26,6 +27,11 @@ def langs_json():
 @app.command()
 def build_lang(lang_code: str) -> None:
     lang_path = Path("docs") / lang_code
+    if not lang_path.is_dir():
+        typer.echo(f"The language translation doesn't seem to exist yet: {lang_code}")
+        raise typer.Abort()
+    typer.echo(f"Building docs for: {lang_code}")
+    build_site_dist_path = build_site_path / lang_code
     if lang_code == default_lang:
         dist_path = site_path
     else:
@@ -33,8 +39,11 @@ def build_lang(lang_code: str) -> None:
         shutil.rmtree(dist_path, ignore_errors=True)
     current_dir = os.getcwd()
     os.chdir(lang_path)
-    subprocess.run(["mkdocs", "build", "--site-dir", dist_path], check=True)
+    shutil.rmtree(build_site_dist_path, ignore_errors=True)
+    subprocess.run(["mkdocs", "build", "--site-dir", build_site_dist_path], check=True)
+    shutil.copytree(build_site_dist_path, dist_path, dirs_exist_ok=True)
     os.chdir(current_dir)
+    typer.secho(f"Successfully built docs for: {lang}", color=typer.colors.GREEN)
 
 if __name__ == "__main__":
     app()
